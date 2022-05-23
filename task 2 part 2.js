3f00bc8f42dffde798d8047f9c6489b4e2582ea7
Muthu  R <106005632+Muthu53@users.noreply.github.com>
2022-05-23 15:52:17 +0530

    Json format

---
+++ task 2 part 2.js
@@ -0,0 +1,80 @@
+2)RESUME IN JSON FORMAT->
+----------------------------------------------------------------------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>
+{ 
+ "basics": { 
+ "name": "Muthu  R", 
+ "email": "369rmm@gmail.com", 
+ "phone": "9125554321", 
+ "date of birth": "26-01-1999", 
+ "nationality": "INDIAN", 
+ "url": "https://ankesaiprasad.com", 
+ "summary": "A summary of ankesaiprasad", 
+ "location": { 
+ "address": "MAHATMA GANDHI COLONY", 
+ "postalCode": "515001", 
+ "city": "anantapur", 
+ "countryCode": "IND", 
+  
+  
+ }, 
+ "objective": "A objective of ankesaiprasad", 
+ "carrer objective": { 
+ "Objective": "to be part of orginatizition where i could enchance myskills for devolpment  of organization and myself " 
+ }, 
+ "INTERNSHIP": [{ 
+ "name": "STOCK MANAGEMET USING IOT", 
+ "url": "https://STOCK.com", 
+ "startDate": "2020-01-01", 
+ "endDate": "2020-01-31", 
+ "summary": "Description…", 
+ "highlights": [
+ "done internship" 
+ ] 
+ }], 
+ "education": [{ 
+ "institution": "RJG INSTIUTE OF TECHNOLOGY",  "url": "https://RJGinstitution.com/", 
+ "area": "pondicherry", 
+ "studyType": "Bachelor", 
+ "startDate": "2017-06-26", 
+ "endDate": "8-10-2021", 
+ "score": "",7.33 
+ "courses": [ 
+ "Bachelor of engineering - B.E" 
+ ] 
+ }], 
+ "10th standard": [{ 
+ "college": "BCET",  "url": "https://BCETinstitution.com/", 
+ "area": "karaikal", 
+ "year": "2014-2016", 
+ "score": "679" 
+ }], 
+ "12th standard": [{ 
+ "school": "Rotary concept school", 
+ "url": "https://rtyinstitution.com/", 
+ "area": "thirumanjanavidhi", 
+ "year": "2017", 
+ "score": "7"
+ }], 
+ "final year project": [{ 
+ "name": "portable health care system using iot",  "technology": "Iot", 
+ "keywords": [ 
+ "HTML", 
+ "CSS", 
+ "JavaScript" 
+ ] 
+ }], 
+ "languages known": [{ 
+ "language": "English", 
+ "fluency": "Native speaker" 
+ }], 
+ "interests": [{ 
+ "name": "reading books", 
+ "keywords": [ 
+ "maha bharatha", 
+ "Modern India" 
+ ] 
+ }],
+ "declaration": [{ 
+ "declaration": "Muthu R,here by declare that the above info is true to rest of my  knowledge ", 
+ }], 
+  
